57a5284a95a0eb8f1e7ad03be8ca0cf50064a9b5
Kunwardev <kunwardev94@gmail.com>
2021-03-31 15:06:37 -0400

    Skeleton of the Project

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            var users = _context.Users.ToListAsync();
+            return await users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            var user = _context.Users.Find(id);
+            return await user;
+        }
+
+    }
+}
